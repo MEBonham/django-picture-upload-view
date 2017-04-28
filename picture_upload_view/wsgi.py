196e855758118984e33692489562d9eb24f57063
McKay Bonham <programmer@mckaybonham.com>
2017-04-28 18:18:46 +0000

    Website loads

---
+++ picture_upload_view/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for picture_upload_view project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picture_upload_view.settings")
+
+application = get_wsgi_application()
